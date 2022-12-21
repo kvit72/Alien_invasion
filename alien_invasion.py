b35473a75ac1e8d29b017dfbd4a1ae8253448357
kvit7 <kvit72v@yandex.ru>
2022-12-21 16:11:57 +0300

    Создание классов для управлением игрой, настройками, кораблем. Добавлено изображение корабля.

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            # Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # При каждом проходе цикла перерысовывается экран.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # Отображение последнего прорисованного экрана.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
