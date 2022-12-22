1ff0041a1744667297a8939578ad4680300447c1
kvit7 <kvit72v@yandex.ru>
2022-12-22 11:05:23 +0300

    Создание пришельца.

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
-
+from alien import Alien
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -18,7 +18,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
-
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
 
     def run_game(self):
         """Запуск основного цикла игры."""
@@ -74,6 +75,12 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
             print(len(self.bullets))
 
+    def _create_fleet(self):
+        """Создание флота вторжения."""
+        # Создание пришельца.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
@@ -81,6 +88,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
